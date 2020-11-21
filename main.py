42bce2c7cf2b86bd4e86ba3b3af8f811bcfc00eb
he10910 <>
2020-11-21 11:48:38 +0800

    美化

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ student = jdata["student"]    #自定義人名
 
 allsub = len(subject)
 allstu = len(student)
-#sublist = " ".join(subject)
 stulist = " ".join(str(student))
 
 
 def sublist():
     for i in range(allsub) :
-        print("{:10s}".format(str(subject[i])),end="")
+        print("{:^10s}".format(str(subject[i])),end="")
     return ""
 
 reportcard = []                      #成績初始化
@@ -22,7 +21,7 @@ for i in range(allstu):
 
 for i in range(allstu):
     for j in range(allsub):
-        reportcard[i][j] = input(f"{student[i]}的{subject[j]}:")  #設定數值
+        reportcard[i][j] = input(f"{student[i]} {subject[j]}:")  #設定數值
 print("-"*10)
 
 def subava(sub):                            #獲得單科平均
@@ -38,7 +37,7 @@ tmplist = []                           #製作科目平均列表
 while n+1 <= allsub:
     tmplist.append("{:^10s}".format(str(subava(n))))
     n += 1
-subavelist = " ".join(tmplist)
+subavelist = "".join(tmplist)
 
 
 def scoreline(whos):                           #獲得成績排序
@@ -64,17 +63,17 @@ allsubplus = s
 allsubave = "{:.2f}".format(allsubplus/(allsub*allstu))     #全科平均
 
 
-print("{:^10s}".format("名字"),end = "")             #第一行title
+print("{:^10s}".format("name"),end = "")             #第一行title
 print(sublist(),end = "")
-print("{:10s}{:10s}".format("總分","平均")) 
+print("{:^10s}{:^10s}".format("total","avarage")) 
 
 for i in range(allstu):                        #中間
     print("{:^10s}".format(student[i]),end = "")
     print(scoreline(i),end = "")
     print("{:^10d}".format(allplus(i)),end = "")  
-    print("{:^.2f}".format(allplus(i)/allsub))
+    print("{:^10.2f}".format(allplus(i)/allsub))
 
-print("{:^10s}".format("平均"),end = "")   #最後
+print("{:^10s}".format("avarage"),end = "")   #最後
 print(subavelist,end = "")
 print("{:^10s}".format("XX"),end = "")
 print("{:^10s}".format(allsubave))
